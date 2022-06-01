fdf3b98eae9f115fed76151c3da5e0eeb4d8e86c
Irene <46569913+Ninfadora@users.noreply.github.com>
2022-06-01 09:04:29 +0200

    Update monitor.py
    Should work

--- lambdas/monitor.py
+++ lambdas/monitor.py
@@ -8,6 +8,12 @@ import boto3
 s3 = boto3.client('s3')
 client = boto3.client("s3")
 
+FILENAME = "sensors.json"
+BUCKET = "myjsonbucketest"
+
+sensor_name = ["p","t"]
+area = ["industrial", "residential"]
+customer = ["AB-Service", "Atlanta Group"]
 
 class DeviceInfo:
     def __init__(self, device_id, device_healt, type, area, customer):
@@ -22,26 +28,20 @@ class DeviceInfo:
 
 def createSensor():
 
-    SENSOR_NUMBER = int(os.environ['SENSOR_NUMBER'])
-    filename = "sensors.json"
-    mybucket = "myjsonbucketest"
-    
-    sensor_name = ["p","t"]
-    area = ["industrial", "residential"]
-    customer = ["AB-Service", "Atlanta Group"]
+    SENSORS_NUMBER = int(os.environ['SENSORS_NUMBER'])
     device_info = []
     
     i = 0
-    f = open(f'/tmp/{filename}','a')
+    f = open(f'/tmp/{FILENAME}','a')
     
-    while i < SENSOR_NUMBER:
+    while i < SENSORS_NUMBER:
        id = random.choice(sensor_name) + "-" + "".join(random.choice(printable[0:9]) for j in range(8))
-       d = DeviceInfo(id, random.randint(0,10), 'pollution' if i[0:1] == "p" else 'temperature', random.choice(area), random.choice(customer))
+       d = DeviceInfo(id, random.randint(0,10), 'pollution' if id[0:1] == "p" else 'temperature', random.choice(area), random.choice(customer))
        device_info.append(d)
         
        f.write(json.dumps(d.__dict__)+'\n')
         
-       client.upload_file(f'/tmp/{filename}', mybucket, f'json/{filename}')
+       client.upload_file(f'/tmp/{FILENAME}', BUCKET, f'json/{FILENAME}')
        i += 1
      
     f.close()     
