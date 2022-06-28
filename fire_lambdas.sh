3dd840da626c3b8733c901327874d2db33183738
Guglielmo Felici <guglielmo.felici@protonmail.com>
2022-06-28 22:55:08 +0200

    lambda script

--- fire_lambdas.sh
+++ fire_lambdas.sh
@@ -1,4 +1,4 @@
 for (( i=1; i<$1; i++ )); do
-    aws lambda invoke --function-name sensors --payload '{  }' response.json
+    aws lambda invoke --function-name sensors --payload '{  }' response.json &
     sleep $2
 done
